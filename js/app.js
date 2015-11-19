17d38d7a55ac07d307c2b004aa1f52713b17bd5c
unknown <nwa11ac3@gmail.com>
2015-11-19 16:51:35 -0700

    started work on equipment page

--- js/app.js
+++ js/app.js
@@ -4,11 +4,13 @@ app.config(function($routeProvider) {
 
 	$routeProvider
 	.when('/', {
-		templateUrl: 'js/main/main.html',
-		controller: 'mainCtrl'
+		templateUrl: 'js/main/main.html'
+	})
+	.when ('/equipment', {
+		templateUrl: 'js/equipment/equipment.html'
 	})
 	.otherwise({
-		redirectTo: '/'
+		redirectTo: '/equipment'
 	})
 
 })
--- js/main/mainCtrl.js
+++ js/main/mainCtrl.js
@@ -1,10 +1,47 @@
 app.controller('mainCtrl', function($scope, mainService) {
 	$scope.exercises = [];
-	$scope.randomSingle = function() {
-		console.log('getting random');
-		$scope.exercises = [];
-		$scope.exercises.push(mainService.getWorkout(1));
+
+	if (!$scope.equipment) {
+		$scope.equipment = {
+			"Bands": false,
+			"Barbell": false,
+			"Body Only": false,
+			"Cable": false,
+			"Dumbbell": false,
+			"E-Z Curl Bar": false,
+			"Exercise Ball": false,
+			"Foam Roll": false,
+			"Kettlebells": false,
+			"Medecine Ball": false
+		};
 	}
 
-		$scope.randomSingle();
+	$scope.randomSingle = function() {
+		if (!equipmentSelected()) {
+			console.log('No equipment selected');
+			return;
+		}
+		while (true) {
+			ranWorkout = mainService.getWorkout(1);
+			reqEquipment = ranWorkout['Equipment'].trim();
+			if ($scope.equipment[reqEquipment]) {
+				break;
+			}
+		}
+		$scope.exercises = [];
+		$scope.exercises.push(ranWorkout);
+	}
+
+
+	function equipmentSelected() {
+		for (var key in $scope.equipment){
+			if ($scope.equipment[key] === true) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	$scope.randomSingle();
 })
